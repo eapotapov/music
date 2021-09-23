29a9e5103d40ddf06f6acdd955c6356877e7daec
eapotapov <eapotapov@devopsprodigy.com>
2021-09-23 14:51:43 +0300

    chord notes suggestor refactored to oop

--- notes.php
+++ notes.php
@@ -1,86 +1,75 @@
 <?php
-$notes = array('C','D','E','F','G','A','B');
-$notes_indexes = array( 'C'=> 0,'D' => 1, 'E' => 2, 'F' => 3, 'G' => 4, 'A' => 5, 'B' => 6);
-$notes_to_choose = $notes;
-while(true) {
-    if(count($notes_to_choose)==0){
-        $notes_to_choose = $notes;
-    }
-    
-    
-    //$position = rand(0,(count($notes_to_choose)-1));    
 
-
-    //$start = $notes_indexes[$notes_to_choose[$position]];    
-    
-    array_splice($notes_to_choose,$position,1);
-    $reversed = rand(0,0);
-    $reversed_output = '';
-
-    if ($reversed) {
-        $reversed_output = " reversed";
-    }
-    
-    $chord = readline("type 1 3 5 7 of " . $notes[$start] . $reversed_output . ": ");    
-    $chord_array = preg_split('/\s/',$chord);
-    
-    $correct_chord = '';
-    $index = $start;
-    $i = 0;
-    
-    foreach($chord_array as $note) {        
-        if($i==0 && $reversed)
-        {
-            $index = $index-1;
-            if($index<0)
-            $index+7;
-        }
-        $correct_note = $notes[$index];
-        $correct_chord = $correct_chord.$correct_note . " ";
-        if($reversed) {
-            $index = $index - 2;            
-            if ($index<0) {
-                $index = $index + 7;
-            }
-        }
-        
-        else {
-            $index = ($index + 2) % 7;
-        }            
-        $i++;
-    }
-    
-    
-    print $correct_chord. " ".$chord."\n";    
-
-    function extract_random_element($array)
-    {
-        $element = false;
-        $position = rand(0,(count($array)-1));    
-        $array_reverse = array_reverse($array);
-        $index = $array_reverse[$array[$position]];    
-        $element = $array[$position];
-        $array = array_splice($array,$position,1);
-        return array($element, $index, $array)
-    }
-    
+$noteExtractor = new NoteExtractor();
+while(true)
+{
+    $chord = $noteExtractor->getChord($noteExtractor->getRandomStep());
+    readline("Type chord with tonic of " . $chord[0].": ");
+    print implode(' ',$chord)."\n";
 }
 
+
+
 class NoteExtractor {
+
+    const CMajor = array('C','D','E','F','G','A','B');
+
     private $initialNotes;
     private $currentNotes;
-    private $notes_indexes;
+    private $notesIndexes;
 
-    private $clearOnEmpty=true;
-    public function __construct($initialArray)
+    private $restartOnEmpty = true;
+    private $clearOnExtract = true; 
+
+    public function __construct( $tonality = false )
     {
-        $this->initialArray = $initialArray;
+        //later we'll update array shift to match the tonality        
+        $this->initialNotes = NoteExtractor::CMajor;
+        $this->currentNotes = $this->initialNotes;
+        $this->notesIndexes = array_flip($this->currentNotes);        
+        
     }
 
-    public function setClearOnEmpty($clearOnEmpty)
+    public function getRandomStep()
     {
-        $this->setClearOnEmpty = $clearOnEmpty;
+        $position = rand(0,(count($this->currentNotes)-1)); 
+        $step = $this->notesIndexes[$this->currentNotes[$position]];        
+        if($this->clearOnExtract)
+        {
+            array_splice($this->currentNotes,$position,1);
+        }
+
+        if($this->restartOnEmpty) 
+        {
+            if ( count($this->currentNotes) == 0 )
+            {
+                $this->currentNotes = $this->initialNotes;
+            }
+        }
+
+        //note that step starts with 0 not with 1        
+        return $step;
     }
+
+    public function getChord($step, $is_reversed = false)
+    {
+        //pls note that step starts with 0, not with 1
+        $chord = array();
+        $index = $step;
+        for($i = 0; $i<4; $i++) 
+        {   
+            array_push($chord,$this->initialNotes[$index]);          
+            $index = $index + 2;                                
+            if ($index > 6)
+            {
+                $index = $index % 7;
+            }   
+                     
+        }
+        return $chord;
+    }
+
+
 
 }
 
